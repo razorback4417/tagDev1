7977233799ca1f88e186a257872c01a1c30d5467
razorback4417 <razorback4417.800@gmail.com>
2024-03-22 14:47:28 -0700

    yarn update

--- metro.config.js
+++ metro.config.js
@@ -6,10 +6,39 @@
 
 // module.exports = config;
 
-const { getDefaultConfig } = require('@react-native/metro-config');
+// const { getDefaultConfig } = require('@react-native/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.sourceExts.push('cjs');
+// const defaultConfig = getDefaultConfig(__dirname);
+// defaultConfig.resolver.sourceExts.push('cjs');
 
-module.exports = defaultConfig;
+// module.exports = defaultConfig;
 
+// const { getDefaultConfig } = require('metro-config');
+
+// module.exports = (async () => {
+//     const {
+//         resolver: { sourceExts, assetExts },
+//     } = await getDefaultConfig();
+//     return {
+//         transformer: {
+//             getTransformOptions: async () => ({
+//                 transform: {
+//                     experimentalImportSupport: false,
+//                     inlineRequires: false,
+//                 },
+//             }),
+//         },
+//         resolver: {
+//             assetExts: assetExts.filter(ext => ext !== 'svg'),
+//             sourceExts: [...sourceExts, 'svg'],
+//         },
+//     };
+// })();
+
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+module.exports = config;
